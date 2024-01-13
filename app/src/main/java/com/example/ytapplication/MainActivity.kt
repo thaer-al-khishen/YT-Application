2c020285fe5442faf6769ff3d10d82ca537c0c2c
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2024-01-13 22:51:45 +0200

    Setup clean slate

--- app/src/main/java/com/example/ytapplication/MainActivity.kt
+++ app/src/main/java/com/example/ytapplication/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.ytapplication.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), FragmentAction {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
@@ -16,33 +16,6 @@ class MainActivity : AppCompatActivity(), FragmentAction {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
-        navController = navHostFragment.navController
-
-    }
-
-    override fun onFirstFragmentButtonClicked(firstFragmentText: String) {
-//        supportFragmentManager.beginTransaction().also { fragmentTransaction ->
-//            fragmentTransaction.replace(binding.fragmentContainerView.id, SecondFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString("key", "Value from first fragment")
-//                }
-//            })
-//            fragmentTransaction.addToBackStack(null)
-//            fragmentTransaction.commit()
-//        }
-        if (navController.currentDestination?.id == R.id.firstFragment) {
-            navController.navigate(FirstFragmentDirections.actionFirstFragmentToSecondFragment(
-                sfInput = SecondFragmentInput(secondFragmentText = firstFragmentText)
-            ))
-        }
-
-    }
-
-    override fun onSecondFragmentButtonClicked() {
-        if (navController.currentDestination?.id == R.id.secondFragment) {
-            navController.navigate(SecondFragmentDirections.actionSecondFragmentToThirdFragment())
-        }
     }
 
 }
