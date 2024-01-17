74ba0f45a26c7ffb954ec7fb3b746019ee188804
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2024-01-17 20:00:07 +0200

    Reset clean slate without binding in activity

--- app/src/main/java/com/example/ytapplication/MainActivity.kt
+++ app/src/main/java/com/example/ytapplication/MainActivity.kt
@@ -8,13 +8,11 @@ import com.example.ytapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+
 
     }
 
