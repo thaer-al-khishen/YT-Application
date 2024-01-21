12f897ff3eed501cb19ba8cb24187bd37724215b
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2024-01-21 20:52:34 +0200

    Initial setup for shared preferences video

--- app/src/main/java/com/example/ytapplication/MainActivity.kt
+++ app/src/main/java/com/example/ytapplication/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.ytapplication
 
-import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ytapplication.databinding.ActivityMainBinding
@@ -15,29 +13,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Load the saved preference
-        val sharedPrefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
-        val isDarkMode = sharedPrefs.getBoolean("DarkMode", false)
 
-        binding.switchDarkMode.isChecked = isDarkMode
-        updateUI(isDarkMode)
 
-        binding.switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
-            // Save the preference
-            sharedPrefs.edit().putBoolean("DarkMode", isChecked).apply()
-            updateUI(isChecked)
-        }
-
-    }
-
-    private fun updateUI(isDarkMode: Boolean) {
-        if (isDarkMode) {
-            binding.textView.setTextColor(Color.WHITE)
-            binding.clParent.setBackgroundColor(Color.BLACK)
-        } else {
-            binding.textView.setTextColor(Color.BLACK)
-            binding.clParent.setBackgroundColor(Color.WHITE)
-        }
     }
 
 }
