84a67a9ce287622fc10e9440a4013e028657d443
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2024-01-21 15:44:42 +0200

    Setup shared preferences

--- app/src/main/java/com/example/ytapplication/MainActivity.kt
+++ app/src/main/java/com/example/ytapplication/MainActivity.kt
@@ -1,19 +1,43 @@
 package com.example.ytapplication
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import com.example.ytapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // Load the saved preference
+        val sharedPrefs = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val isDarkMode = sharedPrefs.getBoolean("DarkMode", false)
 
+        binding.switchDarkMode.isChecked = isDarkMode
+        updateUI(isDarkMode)
 
+        binding.switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
+            // Save the preference
+            sharedPrefs.edit().putBoolean("DarkMode", isChecked).apply()
+            updateUI(isChecked)
+        }
+
+    }
+
+    private fun updateUI(isDarkMode: Boolean) {
+        if (isDarkMode) {
+            binding.textView.setTextColor(Color.WHITE)
+            binding.clParent.setBackgroundColor(Color.BLACK)
+        } else {
+            binding.textView.setTextColor(Color.BLACK)
+            binding.clParent.setBackgroundColor(Color.WHITE)
+        }
     }
 
 }
