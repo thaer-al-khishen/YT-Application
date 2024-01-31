8030ea87199259e69ec5ca91fd4df7df77f541cb
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2024-01-31 22:55:24 +0200

    Foreground services initial setup

--- app/src/main/java/com/example/ytapplication/MainActivity.kt
+++ app/src/main/java/com/example/ytapplication/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.example.ytapplication
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.example.ytapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,28 +13,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 1)
-            }
-        }
-
         binding.btnPlayMusic.setOnClickListener {
-            val serviceIntent = Intent(this, MusicService::class.java).apply {
-                action = ServiceAction.PLAY_MUSIC.toString()
-            }
-            startService(serviceIntent)
+
         }
 
         binding.btnStopPlayingMusic.setOnClickListener {
-            val serviceIntent = Intent(this, MusicService::class.java).apply {
-                action = ServiceAction.STOP_MUSIC.toString()
-            }
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                startForegroundService(serviceIntent)
-            } else {
-                startService(serviceIntent)
-            }
+
         }
 
     }
--- app/src/main/java/com/example/ytapplication/ServiceAction.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ytapplication
-
-enum class ServiceAction {
-    PLAY_MUSIC, STOP_MUSIC
-}
