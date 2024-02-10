94ac78e78d55add5ce90228b1110eb857eb486dc
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2024-02-10 21:10:45 +0200

    Setup coroutines

--- app/src/main/java/com/example/ytapplication/MainActivity.kt
+++ app/src/main/java/com/example/ytapplication/MainActivity.kt
@@ -1,19 +1,52 @@
 package com.example.ytapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import com.example.ytapplication.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.btnLogAction.setOnClickListener {
+            Log.d("ButtonClick", "Clicked on button!")
+        }
 
+        CoroutineScope(Dispatchers.IO).launch {
+            val userData = fetchUserData()
+            processUserData(userData)
+        }
+        CoroutineScope(Dispatchers.IO).launch {
+            fetchNews()
+        }
+    }
 
+    // Simulate fetching user data from a network or database
+    private suspend fun fetchUserData(): String {
+        delay(1000) // Simulates a long-running task, e.g., network call
+        println("Fetched User Data")
+        return "User Data"
+    }
+
+    // Simulate processing the fetched user data
+    private suspend fun processUserData(data: String) {
+        delay(1000) // Simulates processing data
+        println("Processed $data")
+    }
+
+    private suspend fun fetchNews() {
+        delay(1500)
+        println("Today's news")
     }
 
 }
