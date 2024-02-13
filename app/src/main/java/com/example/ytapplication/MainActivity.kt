52e789f8148dadb127d2671ae12a251d01746f4d
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2024-02-13 22:12:54 +0200

    Clean slate remove unused dependencies

--- app/src/main/java/com/example/ytapplication/MainActivity.kt
+++ app/src/main/java/com/example/ytapplication/MainActivity.kt
@@ -2,15 +2,16 @@ package com.example.ytapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import com.example.ytapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
 
