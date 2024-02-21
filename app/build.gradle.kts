0613df3b5e93b690a272aa6a3baa64e60d381792
Thaer Al Khishen <thaer.elkhishen@foo.mobi>
2024-02-21 23:49:16 +0200

    Setup async and await

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 }
--- app/src/main/java/com/example/ytapplication/MainActivity.kt
+++ app/src/main/java/com/example/ytapplication/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.ytapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.ytapplication.databinding.ActivityMainBinding
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +22,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                Log.d("UserDetails", "Launched at: ${System.currentTimeMillis()}")
+                fetchUserDetails().also {
+                    Log.d("UserDetails", "Retrieved at: ${System.currentTimeMillis()}")
+                    Log.d("UserDetails", it.name)
+                    Log.d("UserDetails", it.email)
+                }
+            }
+        }
 
+    }
+
+    data class UserDetails(
+        val name: String,
+        val email: String
+    )
+
+    private suspend fun fetchUserDetails(): UserDetails = coroutineScope {
+        val nameDeferred = async { fetchUserName() } // Fetch user profile
+        val emailDeferred = async { fetchUserEmail() } // Fetch user statistics
+
+        val name = nameDeferred.await() // Wait for the user profile
+        val email = emailDeferred.await() // Wait for the user stats
+
+        UserDetails(name, email) // Combine data into a single object
+    }
+
+    private suspend fun fetchUserName(): String {
+        // Simulate network call
+        delay(1000) // Placeholder for a network call
+        return "John Doe"
+    }
+
+    private suspend fun fetchUserEmail(): String {
+        // Simulate another network call
+        delay(800) // Placeholder for a network call
+        return "john@example.com"
     }
 
 }
